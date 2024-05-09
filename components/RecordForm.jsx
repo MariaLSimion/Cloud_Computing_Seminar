9115673e51d0952a69930b1335626b55f71a434a
MariaLSimion <smarialiana@yahoo.com>
2024-05-09 17:27:14 +0300

    added link tag to nav bar

--- components/RecordForm.jsx
+++ components/RecordForm.jsx
@@ -19,10 +19,15 @@ const RecordForm = (props) => {
     <div className="flex flex-col h-screen">
 <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+          <Link href="/">
+          <div>
+          <a  class="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcdGukF9JaWuBdyA3V5Nx0quqgdQ0VNzORh0rIlexkZg&s" class="h-8" alt="Virtual Shelf Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Virtual Shelf</span>
           </a>
+          </div>
+          </Link>
+         
           <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
