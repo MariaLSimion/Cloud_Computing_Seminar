9169624774e8451a8d577d6014bcaf01cdb066c6
MariaLSimion <smarialiana@yahoo.com>
2024-05-09 13:30:20 +0300

    added vercel link in contact page

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -53,7 +53,7 @@ const Contact = () => {
   </div>
   <div href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Vercel link</h5>
-    <a href="https://www.vercel.com" className="font-normal text-blue-500 hover:underline dark:text-gray-400">https://www.vercel.com</a>
+    <a href="https://cloud-computing-simion-maria-virtual-shelf-emlwsxlhe.vercel.app" className="font-normal text-blue-500 hover:underline dark:text-gray-400">https://www.vercel.com</a>
   </div>
 </div>
 
