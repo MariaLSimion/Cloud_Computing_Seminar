1e9b5beea7365baceb06f1e7646edb41e27bdc19
MariaLSimion <smarialiana@yahoo.com>
2024-05-09 16:23:33 +0300

    edited main page nav bar

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -2,6 +2,7 @@ import { deleteRecord, getRecords } from "@/utils/recordsFunctions";
 import React, { useEffect, useState } from "react";
 import Spinner from "./Spinner";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const MainPage = () => {
   const router = useRouter();
@@ -91,7 +92,7 @@ const MainPage = () => {
 
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="http://localhost:3000/" class="flex items-center space-x-3 rtl:space-x-reverse">
+          <a href="http://localhost:3000/" className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcdGukF9JaWuBdyA3V5Nx0quqgdQ0VNzORh0rIlexkZg&s" class="h-8" alt="Virtual Shelf Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Virtual Shelf</span>
           </a>
@@ -110,11 +111,21 @@ const MainPage = () => {
                 <a href="http://localhost:3000/about" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
               </li>
               <li>
-                <a href="http://localhost:3000/create" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Add a book</a>
-              </li>
+                <Link href = "/create">
+                <div>
+                <a  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Add a book</a>
+            
+                </div>
+                </Link>
+                  </li>
               <li>
-                <a href="http://localhost:3000/contact" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-              </li>
+          <Link href="/contact">
+            <div>
+            <a className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+            </div>
+           
+          </Link>
+        </li>
             </ul>
           </div>
         </div>
